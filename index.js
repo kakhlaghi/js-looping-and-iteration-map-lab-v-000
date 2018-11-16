64c9496e20cb4ea16496a09931803a81f6a04252
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 23:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ function lowerCaseDrivers(someArray){
   })
 }
 
+function nameToAttributes(list) {
+  return list.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
 
-function nameToAttributes(someArray){
-  return someArray.map(function(driver){
-    return `${driver[name]} is from ${driver}.`
-  })
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
